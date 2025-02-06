90dc0cc22326692fc42f856be90a8db7e70e399f
harshadasonawane <talktoharshada3@gmai.com>
2025-02-06 17:46:25 +0530

    Created my first project

--- src/main/java/com/rahulshettyacadamy/pages/LandingPage.java
+++ src/main/java/com/rahulshettyacadamy/pages/LandingPage.java
@@ -1,7 +1,6 @@
 package com.rahulshettyacadamy.pages;
 
 import com.rahulshettyacadamy.utilities.WaitUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
